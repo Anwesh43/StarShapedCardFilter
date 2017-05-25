db6b871c0fd0ed05ee49180d5c7adbc221f0af82
Anwesh43 <anweshthecool0@gmail.com>
2017-05-26 01:39:50 +0530

    Using OnSelectionChangeListener

--- app/src/main/java/com/anwesome/ui/starshapedfiltercarddemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/starshapedfiltercarddemo/MainActivity.java
@@ -5,7 +5,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.anwesome.ui.starshapedfiltercard.OnSelectionChangeListener;
 import com.anwesome.ui.starshapedfiltercard.StarShapedCardList;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +20,18 @@ public class MainActivity extends AppCompatActivity {
         int colors[] = {Color.parseColor("#00838F"),Color.parseColor("#f44336"),Color.parseColor("#F57F17"),Color.parseColor("#D81B60"),Color.parseColor("#1E88E5")};
         StarShapedCardList starShapedCardList = new StarShapedCardList(this);
         for(int i=0;i<colors.length;i++) {
-            starShapedCardList.addCard(bitmap, colors[i],i%4);
+            final int index = i+1;
+            starShapedCardList.addCard(bitmap, colors[i], i % 4, new OnSelectionChangeListener() {
+                @Override
+                public void onSelect() {
+                    Toast.makeText(MainActivity.this, String.format("%d seleccted",index), Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onUnSelect() {
+                    Toast.makeText(MainActivity.this, String.format("%d unselected",index), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
         starShapedCardList.show();
     }
